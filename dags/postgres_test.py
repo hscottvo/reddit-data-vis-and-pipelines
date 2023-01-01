ec7e2b879043833a7b5d872b802a9fc992dc3906
Scott <hscottvo@gmail.com>
2022-12-31 16:38:53 -0800

    Reformatted SQL queries

--- dags/postgres_test.py
+++ dags/postgres_test.py
@@ -18,24 +18,27 @@ with DAG(
         postgres_conn_id="postgres_localhost",
         sql="""--sql
             create table if not exists dag_runs (
-            dt date, 
-            dag_id character varying,
-            primary key (dt, dag_id)
-        )""",
+              dt date, 
+              dag_id character varying,
+              primary key (dt, dag_id)
+            )
+        """,
     )
 
     task2 = PostgresOperator(
         task_id="delete_data_from_table",
         postgres_conn_id="postgres_localhost",
         sql="""--sql
-            delete from dag_runs where dt = '{{ ds }}' and dag_id = '{{dag.dag_id}}';""",
+            delete from dag_runs where dt = '{{ ds }}' and dag_id = '{{dag.dag_id}}';
+        """,
     )
 
     task3 = PostgresOperator(
         task_id="insert_into_table",
         postgres_conn_id="postgres_localhost",
         sql="""--sql
-            insert into dag_runs (dt, dag_id) values ('{{ ds }}', '{{ dag.dag_id }}')""",
+            insert into dag_runs (dt, dag_id) values ('{{ ds }}', '{{ dag.dag_id }}')
+        """,
     )
 
     task1 >> task2 >> task3
