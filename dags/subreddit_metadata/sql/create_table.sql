a4021bbfaa7b475646ad94dd829187803650b53e
Scott <hscottvo@gmail.com>
2023-05-06 22:50:15 -0700

    Adjusted primary key for subreddit subscriber count

--- dags/subreddit_metadata/sql/create_table.sql
+++ dags/subreddit_metadata/sql/create_table.sql
@@ -1,8 +1,8 @@
-create table if not exists subscriber_count (
+create table if not exists subscriber_count(
   sub_count_id serial
   , subreddit varchar
   , sub_count int
   , date date
-  , primary key (sub_count_id)
+  , primary key (sub_count_id , date)
 );
 
