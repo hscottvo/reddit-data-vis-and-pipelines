07d052548a137125ac2c468ebfe982da963dd75a
hscottvo <56327086+hscottvo@users.noreply.github.com>
2023-05-10 13:37:45 -0700

    First push of info to streamlit

---
+++ streamlit_app.py
@@ -0,0 +1,14 @@
+# Imports
+import streamlit as st
+
+# Write a page title
+st.title("Hello World. This is a Title")
+# Subheader
+st.subheader("Here, a subheader")
+
+# Text
+st.text(
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget ligula eu lectus lobortis condimentum."
+)
+# Using st.write
+st.write("This is can be used for text and other features.")
